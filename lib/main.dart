9b68825fee017164fa10df9f6729aa5eaf681e08
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2021-11-17 13:16:28 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 // @dart=2.9
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_online/models/cart_model.dart';
 import 'package:loja_online/models/user_model.dart';
 import 'package:loja_online/screens/home_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
