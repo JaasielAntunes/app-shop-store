575d5d3dbf0071b88ec95a03d3bd1719a4a4d3bd
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2021-11-17 12:16:47 -0300

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,41 @@
 // @dart=2.9
-
 import 'package:flutter/material.dart';
+import 'package:loja_online/models/cart_model.dart';
+import 'package:loja_online/models/user_model.dart';
 import 'package:loja_online/screens/home_screen.dart';
-//import 'package:loja_online/screens/login_screen.dart';
-//import 'package:loja_online/tabs/home_tab.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(new MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inicio Do Projeto',
-      theme: ThemeData(
-        // definição da cor de fundo do app
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141),
-      ),
-      // desativa a "fitinha vermelha debug"
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    // toda alteração no app será modificada pelo ScopedModel
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      // ao trocar usuário refazer o carrinho usando ScopedModelDescedant
+      child: ScopedModelDescendant<UserModel>(builder: (context, child, model) {
+        return ScopedModel<CartModel>(
+          // acesso ao modelo do carrinho
+          model: CartModel(model),
+          child: MaterialApp(
+            title: 'Shop Store',
+            theme: ThemeData(
+              // definição da cor de fundo do app
+              primarySwatch: Colors.blue,
+              primaryColor: Color.fromARGB(255, 4, 125, 141),
+            ),
+            // desativa a "fitinha vermelha" debug
+            debugShowCheckedModeBanner: false,
+            home: HomeScreen(),
+          ),
+        );
+      }),
     );
   }
 }
